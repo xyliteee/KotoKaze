c4f69347acc5e27c3b6601b2b0a78597f5172917
廖贤 <995686792@qq.com>
2024-09-08 21:19:46 +0800

    微小的语法

--- KotoKazeMain/Static/GlobalDate.cs
+++ KotoKazeMain/Static/GlobalDate.cs
@@ -1,16 +1,13 @@
 ﻿using KotoKaze.Dynamic;
 using static FileControl.FileManager.IniManager;
 using System.Windows;
-using TestContent;
 using XyliteeeMainForm;
 using XyliteeeMainForm.Views;
-using FileControl;
-
+#pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
 namespace KotoKaze.Static
 {
     public static class GlobalData
     {
-#pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
         public static MainWindow MainWindowInstance { get; set; }
         public static toolsPage ToolsPageInstance { get; set; }
         public static homePage HomePageInstance { get; set; }
@@ -20,8 +17,8 @@ namespace KotoKaze.Static
         public static BackgroundTaskList<BackgroundTask> TasksList { get; set; } = [];
         public static List<FrameworkElement> MessageBoxList { get; set; } = [];
         public static bool IsRunning { get; set; } = true;
-        public static double RefreshTime = double.Parse(IniFileRead("Application.ini", "SETTING", "REFRESH_TIME"));//单位为秒
-        public static int AnimationLevel = int.Parse(IniFileRead("Application.ini", "SETTING", "ANIMATION_LEVEL"));
+        public static double RefreshTime { get; set; } = double.Parse(IniFileRead("Application.ini", "SETTING", "REFRESH_TIME"));//单位为秒
+        public static int AnimationLevel { get; set; } = int.Parse(IniFileRead("Application.ini", "SETTING", "ANIMATION_LEVEL"));
 
     }
 }
